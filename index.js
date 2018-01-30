c9cd0e06a97d5614540f2ab3aa05ca83f2ccae6d
RomanovichSG <s.g.romanovich@gmail.com>
2018-01-30 22:19:22 +0200

    some working controllers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
-app.use(function(request, response, next){
-    console.log(request.url);
+app.use(function(request, response, next) {
+    console.log('/script' + request.url + '.js');
     next();
 });
-app.get("/", function(request, response){
-    response.sendFile('html/main.html', {root: __dirname });
+app.get(/.*/, function(request, response) {
+    switch (request.url) {
+        case '/': response.sendFile('index.html', {root: __dirname });
+        break;
+        default: response.sendFile('html' + request.url + '.html', {root: __dirname });
+    }
 });
 app.listen(4000);
